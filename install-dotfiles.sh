2c388e8ee79a26ca9243e3e02b3e685b0af53382
als <mrstuzer@gmail.com>
2026-01-31 23:19:24 +0600

    изменил в скрипте ls на find

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -14,5 +14,25 @@ if [ ! -d "$TEMP_DIR/home" ]; then
   exit 1
 fi
 
-echo "Копирование файлов в $HOME_DIR. Проверяем наличие содержимого в директории home перед копированием
-if ls "$TEMP_DIR/home/"* &>/dev/null && ls "$TEMP_DIR/home/.
+echo "Копирование файлов в $HOME_DIR..."
+
+# Проверяем наличие файлов и папок в директории home
+if find "$TEMP_DIR/home" -mindepth 1 -print -quit | grep . > /dev/null; then
+  # Копируем обычные файлы и папки
+  cp -rfv "$TEMP_DIR/home/*" "$HOME_DIR/"
+
+  # Обрабатываем скрытые файлы и папки отдельно
+  for hidden_file in "$TEMP_DIR/home"/.[^.]@(?:.+); do
+    if [[ -e "$hidden_file" ]]; then
+      cp -rfv "$hidden_file" "$HOME_DIR/"
+    fi
+  done
+else
+  echo "Ошибка: Директория 'home' в репозитории пустая."
+  exit 1
+fi
+
+echo "Очистка временных файлов..."
+rm -rf "$TEMP_DIR"
+
+echo "Dotfiles успешно установлены в $HOME_DIR!"
