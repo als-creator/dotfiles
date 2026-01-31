e2cd1042147155e0b23095f4f3951bfc2d0353fe
als <mrstuzer@gmail.com>
2026-01-31 19:06:52 +0600

    fix settings

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -11,5 +11,5 @@ alias cat="bat"
 alias far='sudo far2l'
 alias steamguard='/run/media/als/Work/Distrib/Linux/AppImage/steamguard'
 alias up="epm update && epm full-upgrade"
-alias mirror='sudo reflector --verbose -l 5 -p https --sort rate --save /etc/pacman.d/mirrorlist'
+alias mirror='sudo reflector --verbose --country 'Russia' -l 25 --sort rate --save /etc/pacman.d/mirrorlist'
 uv generate-shell-completion fish | source
--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -6,19 +6,27 @@ REPO_URL="https://github.com/als-creator/dotfiles.git"
 TEMP_DIR="$(mktemp -d)"
 HOME_DIR="$HOME"
 
-echo "📥 Загрузка dotfiles из $REPO_URL..."
+echo "Загрузка dotfiles из $REPO_URL..."
 git clone --depth=1 "$REPO_URL" "$TEMP_DIR" >/dev/null 2>&1
 
 if [ ! -d "$TEMP_DIR/home" ]; then
-  echo "❌ Папка 'home' не найдена в репозитории!"
+  echo "Папка 'home' не найдена в репозитории!"
   exit 1
 fi
 
-echo "📂 Копирование файлов в $HOME_DIR..."
-# -r — рекурсивно, -f — без подтверждения, -v — показывать действия (опционально)
-cp -rf "$TEMP_DIR/home/"* "$HOME_DIR/"
+echo "Копирование файлов в $HOME_DIR..."
 
-echo "🧹 Очистка временных файлов..."
+# Копируем видимые файлы и папки
+cp -rf "$TEMP_DIR/home/*" "$HOME_DIR/"
+
+# Отдельно обрабатываем скрытые файлы и папки
+for hidden_file in "$TEMP_DIR/home"/.[!.]*; do
+  if [[ -e "$hidden_file" ]]; then
+    cp -rf "$hidden_file" "$HOME_DIR/"
+  fi
+done
+
+echo "Очистка временных файлов..."
 rm -rf "$TEMP_DIR"
 
-echo "✅ Dotfiles успешно установлены в $HOME_DIR!"
+echo "Dotfiles успешно установлены в $HOME_DIR!"
